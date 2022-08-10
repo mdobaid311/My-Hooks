1fc5d8f3f83096ce7c118beae9b91237c5c9c706
Mohammed obaid <mddobaid311@gmail.com>
2022-08-10 15:47:41 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <button>-</button>
+      <span>0</span>
+      <button>+</button>
     </div>
   );
 }
